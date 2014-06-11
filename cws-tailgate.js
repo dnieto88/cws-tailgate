9567d25016f091b0d00eed7cc47cf1a27a9ed01a
dannyniceboy <dnieto88@gmail.com>
2014-06-11 11:56:18 -0500

    deployment

--- cws-tailgate.js
+++ cws-tailgate.js
@@ -153,7 +153,7 @@ try {
 }
 module.run(['$templateCache', function($templateCache) {
   $templateCache.put('/cws-tailgate/map/map.html',
-    '<div class="alert-bar"><alert ng-repeat="alert in alerts | limitTo: 1" type="alert.type" close="closeAlert($index)">{{alert.msg}}</alert></div><small class="error" ng-hide="mapctrl.hasLocation">Unable to grab your current locaiton</small><div class="begin" ng-hide="mapctrl.selectedTeam"><div class="row"><div class="small-12 columns"><div class="panel"><p>Select A Team To Begin</p></div></div></div></div><div class="row"><div class="small-12 medium-4 columns end"><select ng-model="mapctrl.selectedTeam" ng-options="t as t for t in mapctrl.teams" ng-change="getTailGates()"><option value="">-- Select a Team--</option></select></div></div><div ng-if="mapctrl.selectedTeam"><div class="row"><google-map center="map.center" zoom="map.zoom" draggable="false" pan="true" class="small-12 large-12 columns end"><marker coords="currentLocationMarker.coords"><marker-label content="&quot;You are Here&quot;" anchor="0 0" class="tailgate-label"></marker-label></marker><marker coords="cwsMarker.coords" icon="cwsMarker.icon"><marker-label content="cwsMarker.label" anchor="0 0" class="tailgate-label"></marker-label></marker><marker coords="busMarker.coords" icon="busMarker.icon"><marker-label content="busMarker.label" anchor="0 0" class="bus-label"></marker-label></marker><markers models="mapctrl.tailgates" coords="\'coords\'" labelcontent="\'name\'" labelanchor="0 0" labelclass="tailgate-label" icon="\'icon\'" click="showDetails(m)"></markers></google-map></div><div class="row"><div class="small-12 medium-6 columns"><div class="panel primary add-team-form"><h2>{{mapctrl.selectedTeam}} Tailgate</h2><p class="alert-panel" ng-hide="mapctrl.tailgates">There are no Tailgate for {{mapctrl.selectedTeam}} yet. Be the first to start the party</p><div ng-repeat="tg in mapctrl.tailgates" class="tailgate-list">{{tg.name}} | {{tg.time | date}} | <a ng-click="removeLocation(tg)">Stop this bus</a></div></div></div><div class="small-12 medium-6 columns"><div class="panel primary add-team-form"><h2>Add Tailgate</h2><form name="form" novalidate=""><div class="row"><label>Name:<input type="text" ng-model="newTailGate.name" ng-required="true" placeholder="Give Your Tail Gate A Name" tab-index="0"></label></div><div class="row"><label>Team:<input type="text" ng-model="newTailGate.team" ng-required="true" placeholder="Your Favorite Team" tab-index="1" typeahead="team for team in mapctrl.teams | filter:$viewValue | limitTo:8" typeahead-editable="false"></label></div><div class="row"><label><input type="checkbox" ng-model="mapctrl.useCurrentLocation" ng-disabled="true">Use Current Location</label></div><div class="row"><button class="expand" ng-click="add(newTailGate)" ng-disabled="form.$invalid">Add Tail Gate</button></div></form></div></div></div></div>');
+    '<div class="alert-bar"><alert ng-repeat="alert in alerts | limitTo: 1" type="alert.type" close="closeAlert($index)">{{alert.msg}}</alert></div><small class="error" ng-hide="mapctrl.hasLocation">Unable to grab your current locaiton</small><div class="begin" ng-hide="mapctrl.selectedTeam"><div class="row"><div class="small-12 columns"><div class="panel"><p>Select A Team To Begin</p></div></div></div></div><div class="row"><div class="small-12 medium-4 columns end"><select ng-model="mapctrl.selectedTeam" ng-options="t as t for t in mapctrl.teams" ng-change="getTailGates()"><option value="">-- Select a Team--</option></select></div></div><div ng-if="mapctrl.selectedTeam"><div class="row"><google-map center="map.center" zoom="map.zoom" draggable="false" pan="false" control="map.control" events="map.events" options="map.options"><marker coords="currentLocationMarker.coords"><marker-label content="&quot;You are Here&quot;" anchor="0 0" class="tailgate-label"></marker-label></marker><marker coords="cwsMarker.coords" icon="cwsMarker.icon"><marker-label content="cwsMarker.label" anchor="0 0" class="tailgate-label"></marker-label></marker><marker coords="busMarker.coords" icon="busMarker.icon"><marker-label content="busMarker.label" anchor="0 0" class="bus-label"></marker-label></marker><markers models="mapctrl.tailgates" coords="\'coords\'" labelcontent="\'name\'" labelanchor="0 0" labelclass="tailgate-label" icon="\'icon\'" click="showDetails(m)"></markers></google-map></div><div class="row"><div class="small-12 medium-6 columns"><div class="panel primary add-team-form"><h2>{{mapctrl.selectedTeam}} Tailgate</h2><p class="alert-panel" ng-hide="mapctrl.tailgates">There are no Tailgate for {{mapctrl.selectedTeam}} yet. Be the first to start the party</p><div ng-repeat="tg in mapctrl.tailgates" class="tailgate-list">{{tg.name}} | {{tg.time | date}} | <a ng-click="removeLocation(tg)">Stop this bus</a></div></div></div><div class="small-12 medium-6 columns"><div class="panel primary add-team-form"><h2>Add Tailgate</h2><form name="form" novalidate=""><div class="row"><label>Name:<input type="text" ng-model="newTailGate.name" ng-required="true" placeholder="Give Your Tail Gate A Name" tab-index="0"></label></div><div class="row"><label>Team:<input type="text" ng-model="newTailGate.team" ng-required="true" placeholder="Your Favorite Team" tab-index="1" typeahead="team for team in mapctrl.teams | filter:$viewValue | limitTo:8" typeahead-editable="false"></label></div><div class="row"><label><input type="checkbox" ng-model="mapctrl.useCurrentLocation" ng-disabled="true">Use Current Location</label></div><div class="row"><button class="expand" ng-click="add(newTailGate)" ng-disabled="form.$invalid">Add Tail Gate</button></div></form></div></div></div></div>');
 }]);
 })();
 
@@ -229,6 +229,8 @@ angular.module('cwsTailgate.map.controller', ['cwsTailgate.map.service', 'google
 
     $scope.alerts = [];
 
+
+
     $scope.closeAlert = function(i) {
       $scope.alerts.splice(i, 1);
     };
@@ -238,9 +240,23 @@ angular.module('cwsTailgate.map.controller', ['cwsTailgate.map.service', 'google
         latitude: '41.1757071',
         longitude: '-96.01572449999999',
       },
-      zoom: 13
+      zoom: 13,
+      control: {},
+      events: {
+        tilesloaded: function(map) {
+          $scope.$apply(function() {
+         
+      
+            $log.info('this is the map instance', map);
+          });
+        }
+      },
+      options: {
+        panControl: true
+      }
     };
 
+ 
     $scope.mapctrl = {
       tailgates: [],
       currentLocation: {},
@@ -311,8 +327,14 @@ angular.module('cwsTailgate.map.controller', ['cwsTailgate.map.service', 'google
         $scope.currentLocationMarker = success;
         $scope.map.center = success.coords;
       }, function(err) {
-        $scope.mapctrl.currentLocation = $scope.busMarker;
-        $scope.map.center = $scope.busMarker.coords;
+        angular.copy($scope.busMarker, $scope.mapctrl.currentLocation);
+        angular.copy($scope.mapctrl.currentLocation, $scope.busMarker);
+        //$scope.mapctrl.currentLocation = $scope.busMarker;
+        $scope.map.center = {
+          latitude: $scope.busMarker.coords.latitude,
+          longitude: $scope.busMarker.coords.longitude
+        };
+        
         $log.error(err);
       });
     };
